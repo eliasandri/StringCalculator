97a3fbe41bc5fd45cf0884ee7edc621dab26a2ac
Elias Andri <eliasandriasgeirsson@gmail.com>
2018-10-11 17:36:11 +0000

    Refactoring. Took some functionality from the add function and moved it to the calculateSum function which calculates the total sum of some numbers.

--- src/calculator.js
+++ src/calculator.js
@@ -7,19 +7,24 @@ function add(numbers){
   if(numbers.includes(","))
   {
     var numberArray = numbers.split(",")
-    const _ = require("underscore");
-    var sum = 0;
-
-    _.each(numberArray, function (number) {
-        var n = parseInt(number);
-        sum += n;
-      });
+    var sum = calculateSum(numberArray);
     return sum;
   }
 
   else
 
   return parseInt(numbers);
+}
+
+function calculateSum(numbers) {
+  const _ = require("underscore");
+  var sum = 0;
+
+  _.each(numbers, function (number) {
+      var n = parseInt(number);
+      sum += n;
+    });
+  return sum;
 }
 
 module.exports = add;
